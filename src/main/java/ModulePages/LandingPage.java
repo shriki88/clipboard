3c5d888d0e308bc17519b3fdcfcc678685308ced
shrikantsjoshi88 <shrikantsjoshi88@gmail.com>
2022-05-25 14:49:17 +0530

    Added reporting mechanism

--- src/main/java/ModulePages/LandingPage.java
+++ src/main/java/ModulePages/LandingPage.java
@@ -11,7 +11,7 @@ public class LandingPage extends CommonUtilities {
     By HAMBURGER_ICON = By.xpath("//*[@id='nav-hamburger-menu']/i");
     By SHOP_BY_DEPT_LABEL = By.xpath("//*[@id='hmenu-content']/ul[1]/li[14]/div");
     By TV_APP_ELE_SECTION = By.xpath("//*[@id='hmenu-content']/ul[1]/li[16]/a/div");
-    By TELEVISION_SECTION_LABEL = By.xpath("//*[@id='hmenu-content']/ul[9]/li[3]/a");
+    By TELEVISION_SECTION_LABEL = By.xpath("//*[contains(text(),'Televisions')]");
 
 
     public WebDriver loginToApplication() {
@@ -20,22 +20,18 @@ public class LandingPage extends CommonUtilities {
         {
             Reporter.log("Landing page displayed successfully", true);
         }
-        sleep(3000);
-        return driver();
+        return getDriver();
     }
 
     public WebDriver navigateToTVSection() {
         clickElement(HAMBURGER_ICON);
-        sleep(1000);
         Reporter.log("Sections are displayed successfully", true);
         isDisplayed(SHOP_BY_DEPT_LABEL);
         isDisplayed(TV_APP_ELE_SECTION);
         clickElement(TV_APP_ELE_SECTION);
-        waitForPageLoad();
         isDisplayed(TELEVISION_SECTION_LABEL);
         clickElement(TELEVISION_SECTION_LABEL);
-        waitForPageLoad();
-        return driver();
+        return getDriver();
     }
 
 }
--- src/main/java/ModulePages/TelevisionPage.java
+++ src/main/java/ModulePages/TelevisionPage.java
@@ -9,27 +9,28 @@ public class TelevisionPage extends CommonUtilities {
 
     By BRANDS_LABEL = By.xpath("//*[@id='s-refinements']/div[18]/div/span");
     By CHECKBOX = By.xpath("//*[@id='s-refinements']/div[18]/ul/li[4]/span/a/div/label/i");
-    By SORT_DROPDOWN = By.xpath("//*[@id='a-autoid-0']/span/i");
-    By LIST = By.xpath("//*[@id='a-popover-2']/div/div/ul");
+    By SORT_DROPDOWN = By.xpath("//*[@id='a-autoid-0-announce']");
+    By LIST = By.xpath("//*[@id='s-result-sort-select_2']");
     By COST_PRICE = By.xpath("//*[@class='a-price-whole']");
     By ABOUT_THIS = By.xpath("//*[@id='feature-bullets']/h1");
-    By PRICE = By.xpath("//*[contains(text(),'"+P+"')]");
+    By PRICE = By.xpath("//*[contains(text()," + productPrice + ")]");
 
     public WebDriver Televisions() {
         getCurrentURL("television");
-        return driver();
+        Reporter.log("Television page displayed successfully",true);
+        return getDriver();
     }
 
     public WebDriver selectSecondHighestPricedTelevision() {
         moveToElement(BRANDS_LABEL);
         waitForPageLoad();
         clickElement(CHECKBOX);
-        sleep(1000);
+        waitForPageLoad();
         clickElement(SORT_DROPDOWN);
-        selectOption(LIST, "Price: High to Low");
+        clickElement(LIST);
         sortPrice(COST_PRICE);
-        switchToWindow(PRICE,ABOUT_THIS);
-        return driver();
+        switchToWindow(PRICE, ABOUT_THIS);
+        return getDriver();
     }
 
 
